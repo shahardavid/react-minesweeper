899e98c879c68708f3c573e613bd17f0d54f31aa
Shahar David <shahard333@gmail.com>
2018-10-31 22:23:15 +0200

    Updates

---
+++ precache-manifest.1bc5eb0ae134999b51c55ea1277989e9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "973c3c1c7e4ff8016848187a7b924b72",
+    "url": "/react-minesweeper/static/media/superman_symbol_gray.973c3c1c.svg"
+  },
+  {
+    "revision": "568c08204b6d27e92ec8fb61bddd402a",
+    "url": "/react-minesweeper/static/media/superman_symbol_color.568c0820.svg"
+  },
+  {
+    "revision": "88f41955c9f7469a0550ecec76df5510",
+    "url": "/react-minesweeper/static/media/flag.88f41955.svg"
+  },
+  {
+    "revision": "23161bf2efe12b38b71f1865ecd71ab9",
+    "url": "/react-minesweeper/static/media/bomb.23161bf2.svg"
+  },
+  {
+    "revision": "1c80af3cb660a53b4a2e",
+    "url": "/react-minesweeper/static/js/runtime~main.1c80af3c.js"
+  },
+  {
+    "revision": "2b03fe0709a6cb886889",
+    "url": "/react-minesweeper/static/js/main.2b03fe07.chunk.js"
+  },
+  {
+    "revision": "4f6bffe04b4b89d7d5f4",
+    "url": "/react-minesweeper/static/js/1.4f6bffe0.chunk.js"
+  },
+  {
+    "revision": "2b03fe0709a6cb886889",
+    "url": "/react-minesweeper/static/css/main.5b8e272b.chunk.css"
+  },
+  {
+    "revision": "6c93bd8175f09be9919d423d34f9db12",
+    "url": "/react-minesweeper/index.html"
+  }
+];
